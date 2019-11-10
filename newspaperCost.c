73bb38a8dc9c589521b6ab7c2a84e0a50bf2d602
JST <57541376+JSteveT@users.noreply.github.com>
2019-11-10 13:50:01 +0000

    Create newspaperCost.c

---
+++ newspaperCost.c
@@ -0,0 +1,39 @@
+double newspaperCost(int room){
+    double cost = 0;
+    if (room == 1){
+        if(newspaper1 == 1) {
+            cost = 5.5;
+            return cost;
+        }
+        else{
+            return cost;
+        }
+    }
+    else if (room == 2){
+        if(newspaper2 == 1) {
+            cost = 5.5;
+            return cost;
+        }
+        else{
+            return cost;
+        }
+    }
+    else if (room == 3){
+        if(newspaper3 == 1) {
+            cost = 5.5;
+            return cost;
+        }
+        else{
+            return cost;
+        }
+    }
+    else if (room == 4){
+        if(newspaper4 == 1) {
+            cost = 5.5;
+            return cost;
+        }
+        else{
+            return cost;
+        }
+    }
+}
